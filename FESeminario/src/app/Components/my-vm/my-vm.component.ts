c11ddeaa2497cddcee9191fd3ab51452d07d508f
EstebanPelaez <87784235+EstebanPelaez@users.noreply.github.com>
2023-10-20 01:10:26 -0500

    Componentes del Proyecto
    Se implemento el modulo nav-bar y el modulo de gestión de las máquinas virtuales de los usuarios

---
+++ FESeminario/src/app/Components/my-vm/my-vm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-vm',
+  templateUrl: './my-vm.component.html',
+  styleUrls: ['./my-vm.component.css']
+})
+export class MyVMComponent {
+
+}
--- FESeminario/src/app/app.module.ts
+++ FESeminario/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MyVMComponent } from './Components/my-vm/my-vm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    MyVMComponent
   ],
   imports: [
     BrowserModule
