2b212f9c79ed611df1ea53854a7eb6b4ec544342
TCamilo15 <juanc.tamayoa@uqvirtual.edu.co>
2023-10-19 20:32:28 -0500

    Creación del proyecto
    Se crea el proyecto Frontend y dos componentes

---
+++ FESeminario/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
